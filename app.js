3b5d9fee483801533c01c7f422fdee768cfd4d23
Christine de Carteret <Christine@Christines-MacBook-Pro.local>
2016-02-09 15:30:10 -0700

    Fixing app.js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-// Set server port
-app.listen(4000);
-console.log('server is runnin');
-
 //views directory for all template files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //instruct express to server up static assets
 app.use(express.static('public'));
+
+// Set server port
+app.listen(4000);
+console.log('server is runnin');
