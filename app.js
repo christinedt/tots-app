96502f47e330885aa7b78fe014de54a073d40a44
Christine de Carteret <Christine@Christines-MacBook-Pro.local>
2016-02-09 16:04:11 -0700

    Trying to fix this shizz once more

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+//views directory for all template files
+app.set('port', (process.env.PORT || 4000));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//instruct express to server up static assets
+app.use(express.static('public'));
+
 
 //set routes
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
-//views directory for all template files
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-//instruct express to server up static assets
-app.use(express.static('public'));
-
 // Set server port
-app.listen(4000);
-console.log('server is runnin');
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
